dc0e64c1258ccf9de0eda27a8909b3e336a38282
opilane <opilane@HP-CZC6297B9X>
2021-11-10 12:28:49 +0200

    123

--- Rabota_10.11.py
+++ Rabota_10.11.py
@@ -1,5 +1,25 @@
+from module1 import *
+from keyboard import *
+from math import *
+
 while True:
-    print("Funktsioonid" .center(50, "+") )
-    v=input("arithmetic - A")
-    if v-upper()=="A":
-        arithmetic
+    print("Funktsioonid" .center(50, " ") )
+    v=input("arithmetic - A\n is_year_leap Y\n Season C\n Mida sa tahad?")
+    
+    if v.upper()=="A":
+        arv1=float(input("Esimene arv =>"))
+        arv2=float(input("Teine arv =>"))
+        sym=input("Aritmeetiline Tehing =>")
+        rezult=arithmetic(arv1,arv2,sym)
+        print(rezult)
+
+    elif is_year_leap()=="Y":
+         rezult=is_year_leap(int(input("Sisesta aasta")))
+         print(rezult)
+    elif square()=="B":
+         rezult=square(int(input("Sisesta number")))
+         print(rezult)
+    elif season()=="C":
+        l=int(input("kuu =>")) 
+        otvet=season(l)
+        print(otvet)
--- module1.py
+++ module1.py
@@ -1,4 +1,5 @@
-def arithmetic(a: float, b: float,c: str)
+from math import *
+def arithmetic(a: float, b: float,c: str):
 #Lihtne kalkulaator - простой калькулятор
 #+ - liitmine(сложение)
 #- - Lahutamine(разложение)
@@ -8,3 +9,50 @@ def arithmetic(a: float, b: float,c: str)
 #:param float b: Teine arv
 #:param str c: Aritmeetiline Tehing
 #:rtype float:
+    if c=="+":
+        r=a+b
+    elif c=="-":
+        r=a-b
+    elif c=="*":
+        r=a*b
+    elif c=="/":
+        if b!=0:
+            r=a/b
+        else:
+            print("Div0")
+            r=0.0
+    else:
+        print("Viga")
+        r=0.0
+    return r
+
+def is_year_leap(year: int):
+    """Liigaasta leidmine (Високосный год ищем)
+    Tagastab true kui aasta on liigaasta ja False kui ei ole
+    :param int aasta: Aasata number
+    :rtype bool: Funktsioni vvastus loogilises formaadis
+    """
+    if year%4==0:
+        vastus=True
+    else:
+        vastus=False
+    return vastus 
+def square(s1;float):
+    p=s1*4
+    s=s1*s1
+    d=(s1**2) + (s1**2)
+    print("P=",p \n "S=", s\n"D=" , d )
+
+def season(seson: int):
+    """otsime, millele see kuu kuulub 봄여름가을겨울 그리고 봄 
+    """
+
+    if a=="12" or "1" or "2":
+        r="talv"
+    elif a=="3" or "4" or "5":
+        r="kevad"
+    elif a=="6" or "7" or "8":
+        r="suvi"
+    else:
+        r="sügis"
+    return r
