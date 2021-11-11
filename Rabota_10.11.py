03ce725ad798be5f5222c9fa5076219297f010ad
opilane <opilane@HP-CZC6297B9X>
2021-11-11 09:59:17 +0200

    Opa

--- Rabota_10.11.py
+++ Rabota_10.11.py
@@ -3,8 +3,8 @@ from keyboard import *
 from math import *
 
 while True:
-    print("Funktsioonid" .center(50, " ") )
-    v=input("arithmetic - A\n is_year_leap Y\n Season C\n Mida sa tahad?")
+    print("Funktsioonid" .center(50, "+") )
+    v=input("arithmetic - A\n Yearleep Y\nSeason C\nSquare B\nBank K\nis_prime U\n=>")
     
     if v.upper()=="A":
         arv1=float(input("Esimene arv =>"))
@@ -12,14 +12,23 @@ while True:
         sym=input("Aritmeetiline Tehing =>")
         rezult=arithmetic(arv1,arv2,sym)
         print(rezult)
-
-    elif is_year_leap()=="Y":
-         rezult=is_year_leap(int(input("Sisesta aasta")))
+    if v.upper()=="Y":
+         rezult=Yearleep(int(input("Sisesta aasta")))
          print(rezult)
-    elif square()=="B":
+    
+    if v.upper()=="B":
          rezult=square(int(input("Sisesta number")))
          print(rezult)
-    elif season()=="C":
-        l=int(input("kuu =>")) 
-        otvet=season(l)
+    
+    if v.upper()=="C":
+        otvet=season(int(input("kuu =>"))) 
         print(otvet)
+        
+    if v.upper()=="K":
+        rezult=Bank(float(input("money =>"))) 
+        a=years(int(input("year =>")))
+        print(f"Вы заработаете {rezult} за {year} лет.")
+    
+    if v.upper()=="U":
+         rezult=is_prime(int(input("Sisesta number")))
+         print(rezult)
--- module1.py
+++ module1.py
@@ -1,4 +1,5 @@
-from math import *
+from math import*
+
 def arithmetic(a: float, b: float,c: str):
 #Lihtne kalkulaator - простой калькулятор
 #+ - liitmine(сложение)
@@ -26,33 +27,55 @@ def arithmetic(a: float, b: float,c: str):
         r=0.0
     return r
 
-def is_year_leap(year: int):
-    """Liigaasta leidmine (Високосный год ищем)
-    Tagastab true kui aasta on liigaasta ja False kui ei ole
-    :param int aasta: Aasata number
-    :rtype bool: Funktsioni vvastus loogilises formaadis
-    """
-    if year%4==0:
+def Yearleep(aasta: int):
+
+    #Liigaasta leidmine
+    #Tagastab True kui on liigaasta ja False kui ei ole
+    #:param int aasta:Asta number
+    #:rtype bool:Funktsioni vastus logilises formatis
+   
+    if aasta%4==0:
         vastus=True
     else:
         vastus=False
-    return vastus 
-def square(s1;float):
-    p=s1*4
-    s=s1*s1
-    d=(s1**2) + (s1**2)
-    print("P=",p \n "S=", s\n"D=" , d )
+    return vastus
 
-def season(seson: int):
-    """otsime, millele see kuu kuulub 봄여름가을겨울 그리고 봄 
-    """
+def square(j:float):
+    if j!=0:
+        pop=""
+        p=j*4
+        s=j*j
+        d=sqrt(j*j*j*j)
+        print("P=",p)
+        print("S=", s)
+        print("D=" , d)
 
-    if a=="12" or "1" or "2":
-        r="talv"
-    elif a=="3" or "4" or "5":
-        r="kevad"
-    elif a=="6" or "7" or "8":
-        r="suvi"
+    return pop
+
+def season(a: int):
+    #otsime, millele see kuu kuulub 봄여름가을겨울 그리고 봄
+    #:param int seson: seson
+    #:rtype bool: Funktsiooni vastus seson
+    if a==12 or 1 <= a <= 2:
+        r="See on talv"
+    elif 3 <= a <= 5:
+        r="See on kevad"
+    elif 6 <= a <= 8:
+        r="See on suvi"
+    elif 9 <= a <= 11:
+        r="See on sügis"
     else:
-        r="sügis"
+        r="Ma ei tea see kuu"
     return r
+
+def Bank(a: float , years: int):
+    #otsime, milline on panus
+    #:param float money , years: money
+    #:rtyoe bool: Funktisooni vastus money
+
+    if a>=0:
+        k=years*1.1
+        b=a*k
+    return b
+
+
