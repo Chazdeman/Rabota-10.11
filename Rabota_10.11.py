8961fea5a4825feb6f8789190a883a11d11264cf
opilane <opilane@HP-CZC6297B9X>
2021-11-12 12:03:22 +0200

    2s

--- Rabota_10.11.py
+++ Rabota_10.11.py
@@ -4,7 +4,7 @@ from math import *
 
 while True:
     print("Funktsioonid" .center(50, "+") )
-    v=input("arithmetic - A\n Yearleep Y\nSeason C\nSquare B\nBank K\nis_prime U\n=>")
+    v=input("arithmetic - A\n Yearleep Y\nSeason C\nSquare B\nBank K\nis_prime U\nxor_cipher J\ndate X \n=>")
     
     if v.upper()=="A":
         arv1=float(input("Esimene arv =>"))
@@ -12,6 +12,7 @@ while True:
         sym=input("Aritmeetiline Tehing =>")
         rezult=arithmetic(arv1,arv2,sym)
         print(rezult)
+    
     if v.upper()=="Y":
          rezult=Yearleep(int(input("Sisesta aasta")))
          print(rezult)
@@ -21,14 +22,30 @@ while True:
          print(rezult)
     
     if v.upper()=="C":
-        otvet=season(int(input("kuu =>"))) 
+        otvet=season(int(input("Sisesta kuu =>"))) 
         print(otvet)
         
     if v.upper()=="K":
-        rezult=Bank(float(input("money =>"))) 
-        a=years(int(input("year =>")))
-        print(f"Вы заработаете {rezult} за {year} лет.")
+        money=(float(input("Sisesta money =>")))
+        years=(int(input("year =>")))
+        rezult=Bank(money,years)
+        print(f"Вы заработаете {rezult} за {years} years.")
     
     if v.upper()=="U":
          rezult=is_prime(int(input("Sisesta number")))
+         print(rezult)
+    
+    if v.upper()=="J":
+        print("Kodeerimine".center(60,"+"))
+        rezult=xor_cipher(input("Sisesta text=>"), input("Sisesta võti=>"))
+        print(rezult)
+        print("Dekodeerimine".center(60,"+"))
+        de_rezult=xor_uncipher(rezult, input("Sisesta võti =>"))
+        print(de_rezult)
+
+    if v.upper()=="X":
+         day=(int(input("Sisesta day=>")))
+         month=(int(input("Sisesta month=>")))
+         year=(int(input("Sisesta year=>")))
+         rezult=date(day,month,year)
          print(rezult)
--- module1.py
+++ module1.py
@@ -34,13 +34,17 @@ def Yearleep(aasta: int):
     #:param int aasta:Asta number
     #:rtype bool:Funktsioni vastus logilises formatis
    
-    if aasta%4==0:
-        vastus=True
-    else:
+   if aasta % 4 != 0:
+       vastus=True
+   else:
         vastus=False
-    return vastus
+   return vastus
+
 
 def square(j:float):
+    #leidmine P S D
+    #:Param float number: P S D
+    #:rtype bool: 
     if j!=0:
         pop=""
         p=j*4
@@ -56,6 +60,7 @@ def season(a: int):
     #otsime, millele see kuu kuulub 봄여름가을겨울 그리고 봄
     #:param int seson: seson
     #:rtype bool: Funktsiooni vastus seson
+    #Tagastab season talv või kevad või suvi või sügis 
     if a==12 or 1 <= a <= 2:
         r="See on talv"
     elif 3 <= a <= 5:
@@ -68,14 +73,82 @@ def season(a: int):
         r="Ma ei tea see kuu"
     return r
 
+def is_prime(a: int):
+    #algarve leidmine
+    #:param int number : True or False
+    #:rtype bool: Funktsiooni vastus 1 or 2
+    #Tagastab True kui on algarve ja False kui on midagi muu
+    if a % 2 == 0 and a != 2:
+        return False
+    if a == 0 or a == 1:
+        return False
+    for n in range(3, int(sqrt(a).real) + 1, 2):
+        if a % n == 0:  
+            return False
+    return True  
+
 def Bank(a: float , years: int):
     #otsime, milline on panus
     #:param float money , years: money
     #:rtyoe bool: Funktisooni vastus money
-
     if a>=0:
         k=years*1.1
         b=a*k
-    return b
+    return b 
 
+def xor_cipher(string: str , key: str)->str:
+    """Tavaline sõna kodeeritakse 
+    ord - десятичное число,которое кодирует букву
+    """
+    result = ''
+    temp = int()
+    for i in range(len(string)):
+        temp = ord(string[i]) #näitab sümboli kood
+        for j in range(len(key)):
+            temp ^=ord(key[j])
+        result +=chr(temp)
+    return result
 
+def xor_uncipher(string:str , key: str) -> str:
+    """chr - наоборот берёт число и возвращает букву.
+    """
+    result = ''
+    temp=[]
+    for i in range(len(string)):
+        temp.append(string[i])
+        for j in reversed(range(len(key))):
+            temp[i]= chr(ord(key[j]) ^ ord(temp[i]))
+        result +=temp[i]
+    return result
+
+def date(day: int ,month: int , year: int):
+    """otsime, kas on see data või Ei
+    :param int day, int month , int years : True or False
+    :rtype bool:
+    """
+    # если год високосный
+    if year%4 == 0:
+        # проверяем попадает ли месяц и день в лимиты
+        if month == 2 and 0 < day <= 29:
+            vastus=True
+        elif month in 31 and 0 < day <= 31:
+            vastus=True
+        elif month in 30 and 0 < day <= 30:
+            vastus=True
+        else:
+            vastus=False
+    # если год обычный
+    elif year%4 != 0:
+        # проверяем попадает ли месяц и день в лимиты
+        if month == 2 and 0 < day < 28:
+            vastus=True
+        elif month in 31 and 0 < day <= 31:
+            vastus=True
+        elif month in 30 and 0 < day <= 30:
+            vastus=True
+        else:
+            vastus=False
+    else:
+        vastus=False
+
+    return vastus
